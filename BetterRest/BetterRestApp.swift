8efce1d46bf4b1ceafa180eddfc33651608fbc99
Beto Toro <beto.toro@zemoga.com>
2022-07-06 10:28:23 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Beto Toro on 6/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
